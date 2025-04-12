e35aaa995b06baebcbe2bfba11cbe174c3b797cd
시윤 <eqeee@naver.com>
2025-04-12 17:57:21 +0900

    [infra]S3버킷 2개 생성,프론트엔드 깃허브액션 추가

---
+++ infra/terraformInfra/modules/s3_bucket/main.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  force_destroy = var.force_destroy
+}
+
+resource "aws_s3_bucket_website_configuration" "this"{
+  count = var.enable_website ? 1:0
+  bucket = aws_s3_bucket.this.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = var.error_document
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = var.is_public ? false: true
+  block_public_policy     = var.is_public ? false: true
+  ignore_public_acls      = var.is_public ? false: true
+  restrict_public_buckets = var.is_public ? false: true
+}
+
+resource "aws_s3_bucket_policy" "website_policy" {
+  count  = var.enable_website ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.this.arn}/*"
+    }]
+  })
+}
