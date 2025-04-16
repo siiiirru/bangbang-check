073a9cb3c64c7b12f3542d09cedd5c9a632919a4
시윤 <eqeee@naver.com>
2025-04-16 23:14:58 +0900

    [fix]user_pool_name 중복변수 삭제

--- infra/terraformInfra/modules/cognito/variables.tf
+++ infra/terraformInfra/modules/cognito/variables.tf
@@ -2,11 +2,6 @@ variable "user_pool_name" {
   type = string
 }
 
-variable "user_pool_name" {
-  type        = string
-  description = "Name of the Cognito User Pool"
-}
-
 variable "app_client_name" {
   type        = string
   description = "Name of the App Client"
