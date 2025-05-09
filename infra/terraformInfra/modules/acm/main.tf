3e52b3250fc2f9499873fe8e67d5ea3d0f5e6ddc
시윤 <eqeee@naver.com>
2025-05-10 00:02:38 +0900

    [fix] cloudfront typo correction

--- infra/terraformInfra/modules/acm/main.tf
+++ infra/terraformInfra/modules/acm/main.tf
@@ -59,7 +59,7 @@ resource "aws_acm_certificate" "cloudfront_cert" {
 
 resource "aws_route53_record" "cloudfront_cert_validation" {
     for_each = {
-        for dvo in aws_acm_certificate.loudfront_cert.domain_validation_options : dvo.domain_name => {
+        for dvo in aws_acm_certificate.cloudfront_cert.domain_validation_options : dvo.domain_name => {
         name   = dvo.resource_record_name
         record = dvo.resource_record_value
         type   = dvo.resource_record_type
