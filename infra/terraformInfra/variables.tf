bcd3a4a827f0ea952599e9016a8763849cdccc07
siiiirru <eqeee@naver.com>
2025-05-13 23:37:02 +0900

    [fix]워크플로우 name수정,테라폼 람다함수 변수 주석처리

--- infra/terraformInfra/variables.tf
+++ infra/terraformInfra/variables.tf
@@ -13,15 +13,15 @@ variable "frontend_local_url" {
     default = "http://localhost:3000/"
 }
 
-variable "lambda_functions" {
-    description = "List of Lambda function configurations"
-    type = list(object({
-    name                  = string
-    handler               = string
-    runtime               = string
-    environment_variables = map(string)
-    api_resource_path     = string  # API Gateway의 리소스 경로
-    role_arn              = string  
-    http_method           = string
-    }))
-}
+# variable "lambda_functions" {
+#     description = "List of Lambda function configurations"
+#     type = list(object({
+#     name                  = string
+#     handler               = string
+#     runtime               = string
+#     environment_variables = map(string)
+#     api_resource_path     = string  # API Gateway의 리소스 경로
+#     role_arn              = string  
+#     http_method           = string
+#     }))
+# }
