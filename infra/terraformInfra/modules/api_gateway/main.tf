cc609af0edc9c308b35440f0d0ff983a56edaf1e
siiiirru <eqeee@naver.com>
2025-08-23 22:56:13 +0900

    fix: 매번 변경 때 마다 api 배포

--- infra/terraformInfra/modules/api_gateway/main.tf
+++ infra/terraformInfra/modules/api_gateway/main.tf
@@ -84,7 +84,7 @@ resource "aws_api_gateway_deployment" "this" {
   ]
   rest_api_id = aws_api_gateway_rest_api.this.id
   triggers = {
-    redeployment = sha1(jsonencode(var.lambda_functions))
+    redeployment = sha1(jsonencode([var.lambda_functions,timestamp()]))
   }
   lifecycle {
     create_before_destroy = true
